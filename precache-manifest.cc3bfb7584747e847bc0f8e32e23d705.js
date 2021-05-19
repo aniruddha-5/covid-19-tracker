a1da087e9ad9dd04ad9c87dd634f7b5237e2a7b1
aniruddha-5 <aniruddharouth@gmail.com>
2021-05-20 03:18:34 +0530

    Updates

---
+++ precache-manifest.cc3bfb7584747e847bc0f8e32e23d705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10c0b5c93d9b51045495e63d7023be3",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "01ff27e02219205f7b9b",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b637ef32c34577ae13ed",
+    "url": "/covid-19-tracker/static/css/main.567cbd05.chunk.css"
+  },
+  {
+    "revision": "01ff27e02219205f7b9b",
+    "url": "/covid-19-tracker/static/js/2.7c300910.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-tracker/static/js/2.7c300910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b637ef32c34577ae13ed",
+    "url": "/covid-19-tracker/static/js/main.66ef2d58.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
